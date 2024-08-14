85af8ed86be5142c526f8f5a64538b0a83b4d813
Yurner0 <wilsonskch2@hotmail.com>
2024-08-14 16:11:09 -0300

    added functionalities

--- configurations_class.py
+++ configurations_class.py
@@ -9,27 +9,32 @@ class ConfigManager:
     # MONTHS_NUMBER = wi.get_work_item_variable("months_number")
     
     SEARCH_PHRASE = 'climate'
-    SECTIONS = 'sports'
+    # SECTIONS = ['world & nation', 'california', 'opinion', 'entertainment & arts', 'food']
+    SECTIONS = ['world & nation']
     MONTHS_NUMBER = '1'
 
-    BASE_URL = "https://www.latimes.com/search?q=[search_phrase]&s=0"
+    # &s=1 means order by newest
+    BASE_URL = "https://www.latimes.com/search?q=[search_phrase]&s=1"
 
     PREFIX = '&f0='
+    PAGE_SUFFIX = '&p='
+
     # section codes doesn't have a determined sequence.
     SECTION_CODES = {
         "any": "",
-        "world & nation": "00000168-8694-d5d8-a76d-efddaf000000",
-        "california": "00000163-01e2-d9e5-adef-33e2984a0000",
+        "world & nation": "//input[@value='00000168-8694-d5d8-a76d-efddaf000000']",
+        "california": "//input[@value='00000163-01e2-d9e5-adef-33e2984a0000']",
         "politics": "00000163-01e2-d9e5-adef-33e2d08d0000",
-        "opinion": "00000163-01e3-d9e5-adef-33e359e70000",
-        "entertainment & arts": "00000163-01e3-d9e5-adef-33e330f30000",
+        "opinion": "//input[@value='00000163-01e3-d9e5-adef-33e359e70000']",
+        "entertainment & arts": "//input[@value='00000163-01e3-d9e5-adef-33e330f30000']",
         "archives": "00000174-dc37-dc3d-aff4-dfff225c0001",
         "travel & experiences": "00000168-86b6-d2cb-a969-debe73e10000",
         "science & medicine": "00000168-869a-d5d8-a76d-efdbcc330000",
         "climate & environment": "0000016a-b70e-dd5c-abfe-bf3f7b290000",
         "books": "00000168-8659-d5d8-a76d-efd9b0ca0000",
-        "food": "00000168-8683-d2cb-a969-de8b247e0000",
+        "food": "//input[@value='00000168-8683-d2cb-a969-de8b247e0000']",
 
     }
 
+    
     
--- web_scraper.py
+++ web_scraper.py
@@ -1,33 +1,41 @@
 import logging
-import requests
-import lxml
+from lxml import html
 from bs4 import BeautifulSoup
+from selenium.webdriver.remote.webelement import WebElement
 from selenium import webdriver
 # from RPA.core.webdriver import cache, download, start
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from RPA.FileSystem import FileSystem
 from typing import Tuple, Optional
 from configurations_class import ConfigManager
-# from excel_class import ExcelManager
+from excel_class import ExcelManager
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException, StaleElementReferenceException, ElementNotInteractableException
+from selenium.webdriver.support import expected_conditions as EC
 
 from dateutil.relativedelta import relativedelta
-from datetime import date
-import requests
+from datetime import date, datetime, timezone
 import time
 import re
 import os
 import ssl
 import urllib3
+import requests
 
 class CustomSelenium:
 
+    excel = ExcelManager()
+
     def __init__(self) -> None:
-        self.driver = None
-        self.file = FileSystem()
+        self._driver = None
+        self._options = None
+        self._file = FileSystem()
 
         # Data required by the challenge:
-        self.news_list = {
+        self._news_list = {
             "title": [],
             "date": [],
             "description": [],
@@ -35,9 +43,7 @@ class CustomSelenium:
             "phrase_count": [],
             "contains_money": [],
         }
-        # Create output folder
-        # self.setup_output_folder()
-        # self.set_webdriver()
+
     
     def setup_output_folder(self):
         """ Creates output folder if not exists
@@ -63,26 +69,22 @@ class CustomSelenium:
         return start_date, end_date
 
     def set_chrome_options(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
-        options.add_argument('--disable-web-security')
-        options.add_argument("--start-maximized")
-        options.add_argument('--remote-debugging-port=9222')
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        return options
+        if self._options is None:
+            self._options = ChromeOptions()
+            self._options.add_argument('--headless')
+            self._options.add_argument('--no-sandbox')
+            self._options.add_argument("--disable-extensions")
+            self._options.add_argument("--disable-gpu")
+            self._options.add_argument('--disable-web-security')
+            self._options.add_argument("--start-maximized")
+            self._options.add_argument('--remote-debugging-port=9222')
+            self._options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        return self._options
 
     def set_webdriver(self):
-        # options = self.set_chrome_options()
-        # service = Service(ChromeDriverManager().install())
-        # self.driver = webdriver.Chrome(service=service, options=options)
-
-        options = webdriver.ChromeOptions()
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=options)
-
+        if self._driver is None:
+            self._driver = webdriver.Chrome(options=self._options)
+        return self._driver
         print("WebDriver initialized successfully.")
 
     def set_page_size(self, width:int, height:int):
@@ -97,39 +99,198 @@ class CustomSelenium:
         #"Internal width you want to set+Set "outer frame width" to window size
         target_width = width + (current_window_size["width"] - inner_width)
         target_height = height + (current_window_size["height"] - inner_height)
-        self.driver.set_window_rect(
+        self._driver.set_window_rect(
             width=target_width,
             height=target_height)
 
     def open_url(self, url:str, screenshot:str=None):
         """Opens the browser and navigates to the input URL."""
-        self.driver.get(url)
+        self._driver.get(url)
 
-    def driver_quit(self):
-        if self.driver:
-            self.driver.quit()
+    def close(self):
+        if self._driver is not None:
+            self._driver.close()
+            self._driver = None
 
     def full_page_screenshot(self, url):
-        self.driver.get(url)
-        page_width = self.driver.execute_script('return document.body.scrollWidth')
-        page_height = self.driver.execute_script('return document.body.scrollHeight')
-        self.driver.set_window_size(page_width, page_height)
-        self.driver.save_screenshot('screenshot.png')
-        self.driver.quit()
+        self._driver.get(url)
+        page_width = self._driver.execute_script('return document.body.scrollWidth')
+        page_height = self._driver.execute_script('return document.body.scrollHeight')
+        self._driver.set_window_size(page_width, page_height)
+        self._driver.save_screenshot('screenshot.png')
+        self._driver.quit()
 
+    def put_search_in_url(self) -> str:
+        """ Returns the base url
+        Replaces the auxiliar [search_phrase] with the SEARCH_PHRASE constant from the config_manager.py file.
+        """
+        url = ConfigManager.BASE_URL.replace('[search_phrase]', ConfigManager.SEARCH_PHRASE)
+        return url
+
+    def put_sections_in_url(self, url: str) -> str:
+        """ Returns the site url
+        Embbeds all the input sections in the base url by replacing the auxiliar %2C[sections] with the corresponding section code.
+        The section codes are extracted from the NYTimes site and are constants in the config manager file.
+        This means new sections should be coded to work but the efficiency and robustness increases.
+        """
+        middle = ''
+        for section in (ConfigManager.SECTIONS):
+            print(section)
+            middle = middle + ConfigManager.PREFIX + ConfigManager.SECTION_CODES[section]
+            print(middle)
+        url = url[:-4] + middle + url[-4:]
+        return url
+    
+    def download_image(self, img_url, save_path):
+        """Download an image from a URL and save it to the specified path."""
+        try:
+            response = requests.get(img_url, stream=True)
+            response.raise_for_status()  # Check for HTTP errors
+            with open(save_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f"Image saved to {save_path}")
+        except requests.RequestException as e:
+            print(f"Failed to download image: {e}")
+
+
+    def scrape_and_download_images(self, url):
+        """Scrape the provided URL and download images where class='promo-timestamp' is a number higher than 0."""
+        try:
+            response = requests.get(url)
+            # Parse the page content using lxml
+            tree = html.fromstring(response.content)
+            # Convert lxml tree to BeautifulSoup object for easier processing
+            soup = BeautifulSoup(html.tostring(tree, encoding='unicode'), 'lxml')
+
+            # Loop through elements to find those with class 'promo-timestamp' with a number higher than 0
+            for element in soup.find_all(class_='promo-timestamp'):
+                try:
+                    # Extract and convert the timestamp to an integer
+                    timestamp = int(element.get_text().strip())
+                    if timestamp > 0:
+                        # Assuming the image URL is in an <img> tag within the element or its parent
+                        img_tag = element.find('img')
+                        if img_tag and img_tag.get('src'):
+                            img_url = img_tag['src']
+                            # Download the image
+                            img_name = img_url.split('/')[-1]  # Extract image name from URL
+                            save_path = os.path.join('output', img_name)
+                            self.download_image(img_url, save_path)
+                except ValueError:
+                    # Handle the case where conversion to integer fails
+                    continue
+        except requests.RequestException as e:
+            print(f"Failed to retrieve the URL: {e}")
+
+    def click_element(driver, xpath, timeout=10):
+        """Helper function to click an element with a retry mechanism."""
+        try:
+            element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            element.click()
+            return True
+        except (NoSuchElementException, ElementClickInterceptedException, TimeoutException) as e:
+            print("Error clicking element with XPath" , xpath)
+            return False
+
+    def get_news_title(self, article: WebElement) -> str:
+        """ Returns the Title of the current article by looking for the '.promo-title a' css selector
+        """
+        title_locator = '.promo-title a'
+        title = article.find_element(By.CSS_SELECTOR, title_locator).text
+        return title
+
+    def get_news_description(self, article: WebElement) -> str:
+        """ Returns the Description of the current article by looking for the '.promo-title a' css selector
+        """
+        description_locator = '.promo-description'
+        description = article.find_element(By.CSS_SELECTOR, description_locator).text
+        return description
+    
+    def get_news_date(self, article: WebElement) -> str:
+        """ Returns the Description of the current article by looking for the '.promo-title a' css selector
+        """
+        date_locator = '.promo-timestamp'
+        timestamp_element = article.find_element(By.CSS_SELECTOR, date_locator)
+        timestamp_ms = int(timestamp_element.get_attribute('data-timestamp'))
+        timestamp_seconds = timestamp_ms / 1000
+        date = datetime.fromtimestamp(timestamp_seconds, tz=timezone.utc)
+        date_string = date.strftime('%b. %d, %Y %H:%M:%S')  # Example format: "Aug. 14, 2024 12:34:56"
+        return date_string
     def news_fetch(self):
         ''' Main function that calls the rest of them
         '''
+        self.open_url('https://www.latimes.com/')
+        button_xpath = "//button[@class='flex justify-center items-center h-10 py-0 px-2.5 bg-transparent border-0 text-header-text-color cursor-pointer transition-colors hover:opacity-80 xs-5:px-5 md:w-10 md:p-0 md:ml-2.5 md:border md:border-solid md:border-header-border-color md:rounded-sm lg:ml-3.75']//*[name()='svg']"
+        button = self._driver.find_element(By.XPATH, button_xpath)
+        button.click()
+        search_box_xpath = "//input[@placeholder='Search']"
+        search_box = self._driver.find_element(By.XPATH, search_box_xpath)
+        search_box.send_keys(ConfigManager.SEARCH_PHRASE)
+        confirm_xpath = "//button[@class='flex justify-center items-center transition-colors transition-bg cursor-pointer w-10 p-0 shrink-0 bg-transparent border-0']//*[name()='svg']"
+        confirm = self._driver.find_element(By.XPATH, confirm_xpath)
+        confirm.click()
 
-my_driver = CustomSelenium()
-# my_driver.set_webdriver()
-my_driver.setup_output_folder()
-my_driver.driver_quit()
+        
+        
+        time.sleep(8)
+        
+        for section in ConfigManager.SECTIONS:
+            filter_xpath = ConfigManager.SECTION_CODES[section]
+            print(section)
+            filter = self._driver.find_element(By.XPATH, filter_xpath)
 
-# my_driver = CustomSelenium()
-# options = my_driver.set_chrome_options()
-# my_driver.set_webdriver()
-# my_link = 'https://gothamist.com/'
-# # my_driver.open_url(url=my_link)
-# my_driver.full_page_screenshot(url = my_link)
-# my_driver.driver_quit()
+            # filter.click()
+            # time.sleep(8)
+            try:
+                # Attempt to click element A
+                time.sleep(8)
+                filter.click()
+                time.sleep(8)
+            except (NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException):
+                # If element A is not found or can't be clicked, click element B first
+                time.sleep(8)
+                see_all = self._driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(4) > ps-search-results-module:nth-child(2) > form:nth-child(1) > div:nth-child(2) > ps-search-filters:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > ps-toggler:nth-child(1) > ps-toggler:nth-child(2) > button:nth-child(2) > span:nth-child(1)")
+                see_all.click()
+                time.sleep(8)
+
+        #         # Now, retry clicking element A
+                filter2 = self._driver.find_element(By.XPATH, filter_xpath)
+                filter2.click()
+                time.sleep(8)
+
+
+        drop_down_newest = self._driver.find_elements(By.TAG_NAME, value="Option")
+        drop_down_newest[1].click()
+        time.sleep(8)
+        #Ok. Time to work!
+
+        promo_elements = self._driver.find_elements(By.CSS_SELECTOR, value='ps-promo.promo')
+
+        for promo in promo_elements:
+            print(promo)
+            title = self.get_news_title(promo)
+            description = self.get_news_description(promo)
+            date = self.get_news_date(promo)
+            self._news_list["title"].append(title)
+            self._news_list["description"].append(description)
+            self._news_list["date"].append(date)
+
+        self.excel.write_in_excel_file(self._news_list)
+
+
+
+
+        
+
+
+cs = CustomSelenium()
+
+# try:
+    # driver = cs.set_webdriver()
+cs.set_webdriver()
+cs.news_fetch()
+    # use the driver in some way here
+# finally:
+    # ensure that driver is properly closed
+    # cs.close()
